fe6d23b2f4cb5cd29758f251603262eb24a04009
thomasheroguelle <totosh93270@gmail.com>
2023-02-28 18:50:39 +0100

    11eme commit : Polymorphisme : - @Override - super - allumerFeux()

--- src/main/java/Voiture.java
+++ src/main/java/Voiture.java
@@ -19,6 +19,23 @@ class Vehicule {
 }
 
 // on fait hériter nos classes Voitures et Bateau à notre classe Mère Vehicule grace a extends
-class Voitures extends Vehicule {}
+class Voitures extends Vehicule {
+    @Override
+    void start() {
+        super.start();
+        allumerFeux();
+    }
+    // on crée notre méthode qui permet d'allumer les feux de la voiture
+    void allumerFeux() {
+        System.out.println("allumage feux");
+    }
+}
 class Bateau extends Vehicule {}
+
+// 1. on souhaite personnaliser la voiture en y ajoutant une fonction "allumerFeux" -> on crée notre méthode qui permet d'allumer les feux de la voiture
+// 2. comme la fonction allumerFeux() n'est jamais appelée, on va appeler cette fonction lors du démarrage de la voiture.
+// Pour ça, on va surcharger la methode start de la classe Véhicule
+// 3. dans ma classe Voiture, pour personnaliser le comportement d'une fonction héritée de la classe Mère, on utilise super() qui fait reference a la classe Mère
+// 4. @Override sur les methodes qu'on surcharge pour dire que c'est une surcharge de méthode de la classe Mère
+
 
