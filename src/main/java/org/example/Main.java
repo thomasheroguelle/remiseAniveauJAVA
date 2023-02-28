4464e0e722199399917f202aeaf646a0b5fb1f91
thomasheroguelle <totosh93270@gmail.com>
2023-02-28 15:06:31 +0100

    Third commit : caster des int en float

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,13 @@ package org.example;
 // TODO : Faire un test
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! My name is Thomas");
-        System.out.println(10 + 10);
+        //System.out.println("Hello world! My name is Thomas");
+        //System.out.println(10 + 10);
+
+        int a = 5;
+        int b = 2;
+        float c = (float) a / b;
+        System.out.println("resultat : " + c);
+
     }
 }
