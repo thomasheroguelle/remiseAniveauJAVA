0a193927b1de7ace8ac5d391f1c0d39a0ac9f596
thomasheroguelle <totosh93270@gmail.com>
2023-02-28 14:20:50 +0100

    First commit : adding Hello World

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+// TODO : Faire un test
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! My name is Thomas");
+    }
+}
