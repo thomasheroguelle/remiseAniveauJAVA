b495a5a00855269a1ea82853d3c7d7efa7494a9e
thomasheroguelle <totosh93270@gmail.com>
2023-02-28 16:04:24 +0100

    6eme commit : Portabilité d'une variable : variables locales et globales

--- src/main/java/testClass.java
+++ src/main/java/testClass.java
@@ -1,5 +1,7 @@
+// variable globale
 public class testClass {
 
+    // variable locale 1
     public static void main(String[] args) {
         String exemple = "hello";
         // méthode toUpperCase qui permet d'afficher en MAJ
@@ -8,5 +10,20 @@ public class testClass {
         // puis le texte avec lequel le remplacer
         exemple = exemple.replace("HELL", "YEAH");
         System.out.println(exemple);
+
+        // portée des variables
+        fonction1();
+        fonction2();
+    }
+    // chaque variable déclarée en Java a une portée, c à d un champ d'accessibilité dans lequel elle peut
+    // etre utilisée
+    static String exemple = "hello";
+    // variable locale 2
+    public static void fonction1() {
+        System.out.println((exemple));
+    }
+    // variable locale 3
+    public static void fonction2() {
+        System.out.println((exemple));
     }
 }
